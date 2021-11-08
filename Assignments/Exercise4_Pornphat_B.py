b06f18948647e6f0c2c9ff3b3ee1787278ad1264
EMp995 <93866638+EMp995@users.noreply.github.com>
2021-11-08 17:05:17 +0700

    Excercise4

---
+++ Assignments/Exercise4_Pornphat_B.py
@@ -0,0 +1,15 @@
+'''
+รับคะแนน
+'''
+FoundEng = float(input("Foundation English score ==> "))
+GenBus = float(input("General Business score ==> "))
+IntroCom = float(input("Introduction to Computer System score ==> "))
+ComPro = float(input("Computer programing score ==> "))
+'''
+แสดงคะแนน
+'''
+print("\n", "-"*3, " Your Score", "-"*3, "\n")
+print("Foundation English : "+str(FoundEng))
+print("General Business : "+str(GenBus))
+print("Introduction to Computer system : "+str(IntroCom))
+print("Computer Programing : "+str(ComPro))
